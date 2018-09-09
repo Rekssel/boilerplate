418c83d0a351480becb234bb63f813517f8e5cd1
Rekssel <rekssel@ya.ru>
2018-09-09 17:28:29 +0700

    Initial commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container test">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+      data: {
+        temp: 12,
+        temp2: 1231
+      },
+      mounted() {
+        console.log('Component mounted.')
+      },
+      methods: {
+        test() {
+          return 1 + this.temp
+        }
+      }
+    }
+</script>
+
+
+<style lang="sass">
+    .test
+        color: red
+
+        &.test2
+            color: green
+</style>
---
+++ resources/views/blog/new.blade.php
@@ -0,0 +1,7 @@
+@extends('_layout.main')
+
+@section('content')
+<div id="app">
+    new
+</div>
+@endsection
